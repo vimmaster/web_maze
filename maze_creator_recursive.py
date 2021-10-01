fff460321e32276a64b4965ba6b8db7c8adf8699
Ricard Sanchez <ricardsanchez50@gmail.com>
2021-10-01 12:02:33 +0200

    Adds recusive working and iterative
    Iterative algorithm doesn't work.

---
+++ maze_creator_recursive.py
@@ -0,0 +1,82 @@
+import random
+import turtle
+from enum import Enum
+import numpy as np
+
+
+class Direccion(Enum):
+    arriba = 1
+    derecha = 2
+    abajo = -1
+    izquierda = -2
+
+    @classmethod
+    def get_inversa(self, direccion):
+        return Direccion(-1 * direccion.value)
+
+
+class Nodo:
+    def __init__(self):
+        self.hijos = []
+        self.direcciones = []
+        self.raiz = False
+
+    def marca_raiz(self):
+        self.raiz = True
+
+
+def mueve(direccion):
+    if direccion == Direccion.arriba:
+        turtle.setheading(90)
+        turtle.forward(paso_pixeles)
+    elif direccion == Direccion.derecha:
+        turtle.setheading(0)
+        turtle.forward(paso_pixeles)
+    elif direccion == Direccion.abajo:
+        turtle.setheading(270)
+        turtle.forward(paso_pixeles)
+    else:
+        turtle.setheading(180)
+        turtle.forward(paso_pixeles)
+
+
+def avanza_retrocede_casilla(nodo_padre, posicion):
+    direcciones_validas = direcciones.copy()
+    for _ in range(len(direcciones)):
+        direccion = random.choice(direcciones_validas)
+        direcciones_validas.remove(direccion)
+        i, j = posicion
+        if direccion == Direccion.arriba:
+            i -= 1
+        elif direccion == Direccion.derecha:
+            j += 1
+        elif direccion == Direccion.abajo:
+            i += 1
+        else:
+            j -= 1
+        posicion_siguiente = i, j
+        if 0 <= i < m and 0 <= j < n and casillas_marcadas[i, j] == False:
+            casillas_marcadas[i, j] = True
+            nodo = Nodo()
+            nodo_padre.hijos.append(nodo)
+            nodo_padre.direcciones.append(direccion)
+            mueve(direccion)
+            del i, j
+            avanza_retrocede_casilla(nodo, posicion_siguiente)
+            mueve(Direccion.get_inversa(direccion))
+
+
+m, n = 10, 10; paso_pixeles = 25
+casillas_marcadas = np.empty((m, n), dtype=bool)
+casillas_marcadas.fill(False)
+inicio = m - 1, n - 1; final = 0, 0
+i, j = inicio
+casillas_marcadas[i, j] = True
+turtle.penup()
+turtle.setpos(50, 50)
+turtle.pendown()
+nodo_raiz = Nodo()
+nodo_raiz.marca_raiz()
+direcciones = list(Direccion)
+avanza_retrocede_casilla(nodo_raiz, inicio)
+turtle.mainloop()
